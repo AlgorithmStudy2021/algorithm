be19fc49ab73aa817418ce8a52090d39b34de4be
hhee00 <78469508+hhee00@users.noreply.github.com>
2021-05-02 19:37:05 +0900

    Add files via upload

---
+++ programmers/level2/2021.04.26~2021.05.02(4주차)/H-index/hhee00.java
@@ -0,0 +1,26 @@
+import java.util.*;
+class Solution {
+    public int solution(int[] citations) {
+        int answer = 0;
+        
+        Arrays.sort(citations);
+        
+        int maxNum = citations[citations.length - 1];
+        
+        for (int i = maxNum; i >= 0; i--) {
+            int count = 0;
+            for (int j = 0 ; j < citations.length; j++) {
+                if (citations[j] >= i) {
+                    count ++;
+                }
+            }
+            
+            if (count >= i) {
+                answer = i;
+                break;
+            }
+        }
+        
+        return answer;
+    }
+}
