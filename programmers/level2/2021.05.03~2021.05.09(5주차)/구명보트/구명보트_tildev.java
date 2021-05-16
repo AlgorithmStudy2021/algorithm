06d4fadfe2c7be4ae471954670fff7b29201bae8
tildev <tildevelove@gmail.com>
2021-05-16 20:51:10 +0900

    Add Solution

---
+++ programmers/level2/2021.05.03~2021.05.09(5주차)/구명보트/구명보트_tildev.java
@@ -0,0 +1,36 @@
+import java.util.Arrays;
+
+class Solution {
+
+  public int solution(int[] people, int limit) {
+    int answer = 0;
+
+    Arrays.sort(people);
+    int startIndex = 0;
+    int endIndex = people.length - 1;
+
+    while (true) {
+      if (people[startIndex] + people[endIndex] <= limit) {
+        startIndex += 1;
+      }
+      endIndex -= 1;
+      answer += 1;
+      if (endIndex < startIndex) {
+        break;
+      }
+    }
+
+    return answer;
+  }
+
+  public static void main(String[] args) {
+    int[] people = {70, 50, 80, 50};
+    int[] people1 = {70, 80, 50};
+    int limit = 100;
+
+    int ans = new Solution().solution(people, limit);
+
+    System.out.println(ans);
+
+  }
+}
---
+++ programmers/level2/2021.05.03~2021.05.09(5주차)/더맵게/더맵게_tildev.java
@@ -0,0 +1,36 @@
+import java.util.PriorityQueue;
+
+class Solution {
+
+  public int solution(int[] scoville, int K) {
+    int answer = 0;
+    PriorityQueue<Integer> sQueue = new PriorityQueue<>();
+
+    for (int s : scoville) {
+      sQueue.offer(s);
+    }
+
+    while (sQueue.peek() < K) {
+      // 모든 음식의 스코빌 지수를 K 이상으로 만들 수 없는 경우에는 -1을 return
+      if (sQueue.size() == 1) {
+        return -1;
+      }
+      int a = sQueue.poll();
+      int b = sQueue.poll();
+
+      sQueue.offer(a + b * 2);
+      answer += 1;
+    }
+
+    return answer;
+  }
+
+  public static void main(String[] args) {
+    int[] scoville = {1, 2, 3, 9, 10, 12};
+    int k = 7;
+    int ans = new Solution().solution(scoville, k);
+
+    System.out.println(ans);
+
+  }
+}
