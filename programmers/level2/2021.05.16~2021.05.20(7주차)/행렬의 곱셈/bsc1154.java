b8110570cdd2f259663517cb836249a6d7680807
bsc1154 <khl1154@naver.com>
2021-05-20 22:55:10 +0900

    Update bsc1154.java

--- programmers/level2/2021.05.16~2021.05.20(7주차)/행렬의 곱셈/bsc1154.java
+++ programmers/level2/2021.05.16~2021.05.20(7주차)/행렬의 곱셈/bsc1154.java
@@ -2,9 +2,9 @@ class Solution {
       public int[][] solution(int[][] arr1, int[][] arr2) {
           int[][] answer = new int[arr1.length][arr2[0].length];
 
-          for(int i = 0 ; i < arr1.length ; ++i){
-              for(int j = 0 ; j < arr2[0].length ; ++j){
-                  for(int k = 0 ; k < arr1[0].length ; ++k) {
+          for(int i = 0 ; i < arr1.length ; ++i){//  3    결과 행수
+              for(int j = 0 ; j < arr2[0].length ; ++j){//2 결과 열수 
+                  for(int k = 0 ; k < arr1[0].length ; ++k) {//2 계산 반복 (곱한수를 +)
                       answer[i][j] += arr1[i][k] * arr2[k][j];
                   }
               }
