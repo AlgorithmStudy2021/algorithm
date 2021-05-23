7ffae4a1ceb051647b07157d3ceb6ac168853b0a
bsc1154 <khl1154@naver.com>
2021-05-23 20:22:26 +0900

    Update bsc1154.java

--- programmers/level2/2021.05.20~2021.05.27(8주차)/피보나치 수/bsc1154.java
+++ programmers/level2/2021.05.20~2021.05.27(8주차)/피보나치 수/bsc1154.java
@@ -1,1 +1,17 @@
-test
+public class {
+  
+  public int solution(int n) {
+	if(n == 1 || n == 2) return 1; // @1
+	
+	int n1 = 1, n2 = 1, sum = 0; // @2
+	for(int i=3; i<=n; i++) { // @3
+		sum = n1 + n2; 
+		sum %= 1234567; 
+		n1 = n2; //n1은 이전 회차의 값이므로 이전 sum값임
+		n2 = sum;//이전수에 
+	}
+	
+	return sum;
+}
+  
+}
