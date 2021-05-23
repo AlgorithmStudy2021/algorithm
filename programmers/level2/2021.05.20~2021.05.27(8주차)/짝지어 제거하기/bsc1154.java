38937cf84d72061fc2cf5a72c8e24dfc891bf00c
bsc1154 <khl1154@naver.com>
2021-05-23 20:38:17 +0900

    Update bsc1154.java

--- programmers/level2/2021.05.20~2021.05.27(8주차)/짝지어 제거하기/bsc1154.java
+++ programmers/level2/2021.05.20~2021.05.27(8주차)/짝지어 제거하기/bsc1154.java
@@ -1,1 +1,12 @@
-test
+import java.util.*;
+public class Solution {
+    public int solution(String s) {
+        Stack<Character> stack = new Stack<>();//LAST IN FIRST OUT
+         
+        for(char c : s.toCharArray()) //문자열을 문자열 배열로 변환
+          if(!stack.isEmpty() && stack.peek() == c) stack.pop(); //같으면 뽑는다.
+          else stack.push(c);//다르면 넣는다.
+         //peek 맨 마지막요소 제거하지 않고 반환
+        return stack.isEmpty() ? 1 : 0;
+    }
+}
