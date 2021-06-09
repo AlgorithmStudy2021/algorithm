b947ab2a592cc1bcf2d7fa5d7517c354c604fcb1
bsc1154 <khl1154@naver.com>
2021-06-09 23:50:41 +0900

    Update bsc1154.java

--- programmers/level2/2021.06.07~2021.06.13(10주차)/영어 끝말잇기/bsc1154.java
+++ programmers/level2/2021.06.07~2021.06.13(10주차)/영어 끝말잇기/bsc1154.java
@@ -1,1 +1,23 @@
-test
+import java.util.*;
+class Solution {
+    public int[] solution(int n, String[] words) {
+		        int[] answer = new int[] {0,0};
+		        HashSet<String> hs = new HashSet();
+		        int size = words.length;;
+		        int a=1,b=1;
+		        char c = words[0].charAt(0);
+		        for( String word : words) {
+		        	if(hs.contains(word) || c!=word.charAt(0)) {
+		        		return new int[] {a,b}; 
+		        	}else {
+                        hs.add(word);
+		        		c = word.charAt(word.length()-1);
+		        		if(++a > n) {
+		        			a = 1; 
+		        			b++;
+		        		}
+		        	}
+		        }
+		        return answer;
+		    }
+}
