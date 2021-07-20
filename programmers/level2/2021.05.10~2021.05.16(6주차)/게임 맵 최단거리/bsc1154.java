bd81cb189c5f69a48615a2523a9365be273283a4
bsc1154 <khl1154@naver.com>
2021-07-20 22:33:54 +0900

    Update bsc1154.java

--- programmers/level2/2021.05.10~2021.05.16(6주차)/게임 맵 최단거리/bsc1154.java
+++ programmers/level2/2021.05.10~2021.05.16(6주차)/게임 맵 최단거리/bsc1154.java
@@ -1,61 +1,59 @@
+package test;
+
 import java.util.LinkedList;
 import java.util.Queue;
 
-class Solution {
-	class Position {
-		int x, y;
-		
-		public Position(int x, int y) {
-			super();
-			this.x = x;
-			this.y = y;
-		}
+class gameMap {
 
-		boolean isValid(int width, int height) {
-			if (x < 0 || x >= width) return false;
-			if (y < 0 || y >= height) return false;
-			return true;
-		}
-	}
-	
-	public int solution(int[][] maps) {
-		// BFS : Queue
-		
-		int mapHeight = maps.length;
-		int mapWidth = maps[0].length;
-		Queue<Position> queue = new LinkedList<>();
-		int[][] count = new int[mapHeight][mapWidth]; // 최단거리를 구하기 위한 변수
-		boolean[][] visited = new boolean[mapHeight][mapWidth]; // 지나온 길인지 아닌지 확인을 위한 변수
-		
-		queue.offer(new Position(0, 0));
-		count[0][0] = 1;  
-		visited[0][0] = true;
-		
-        // queue에 데이터가 존재할때까지 반복한다.
-        // 먼저 도착하는 쪽이 visited를 true로 바꿔놓았기 때문에 queue는 결국 비어질 수 밖에 없다.
-		while (!queue.isEmpty()) {
-			Position current = queue.poll();
-			
-			int currentCount = count[current.y][current.x]; 
-			
-			//4가지 경우(동,서,남,북)
-			final int[][] moving = {{0, -1}, {0, 1}, {-1, 0}, {1, 0}};
-			for (int i=0; i<moving.length; i++) {
-				Position moved = new Position(current.x + moving[i][0], current.y + moving[i][1]);
-				if (!moved.isValid(mapWidth, mapHeight)) continue;
-				if (visited[moved.y][moved.x]) continue;
-				if (maps[moved.y][moved.x] == 0) continue; // 0: 벽, 1: 길
-				
-				// 방문하지 않은 위치 && 벽이 아닌 경우 && x축 범위를 넘어서지 않은 경우, y축 범위를 넘어서지 않은 경우
-				count[moved.y][moved.x] = currentCount + 1;
-				visited[moved.y][moved.x] = true;
-				queue.offer(moved);
-			}
-		}
-		
-		int answer = count[mapHeight-1][mapWidth-1];
-		if (answer == 0) return -1;
-		
-		return answer;
-	}
+    static boolean [][] chk;
+    static int [] diry = {-1,0,1,0};//상 우 하 좌
+    static int [] dirx = {0,1,0,-1};
+    static int [][] map;
+    static int n;
+    static int m;
+
+    public class Node{
+        int x;
+        int y;
+
+        public Node(int y, int x){
+            this.y=y;
+            this.x=x;
+        }
+    }//상하 좌우 저장
+
+    public int solution(int[][] maps) {
+        int answer = 0;
+        n = maps.length;
+        m = maps[0].length;
+        chk = new boolean[n][m];//방문지점을 구별하기 위한 배열
+        //행렬 n*m 방문한적이 있는지 저장하기 위한 배열
+        chk[0][0] = true;
+        bfs(0,0);
+
+        answer = map[n-1][m-1];
+        return answer>1 ? map[n-1][m-1] : -1 ;
+    }
+
+    public  void bfs(int y, int x) {
+        Queue<Node> queue = new LinkedList<>();
+        queue.add(new Node(y, x)); //큐에 0.0 추가
+
+        while (!queue.isEmpty()) {
+            Node now = queue.poll();//제일 꼭대기 큘르 꺼내고
+            for (int i = 0; i < 4; i++) {
+                int now_y = now.y + diry[i]; //꼭대기 큐의 x에
+                int now_x = now.x + dirx[i];
+                if (0 <= now_y && now_y < n && 0 <= now_x && now_x < m) {//현재이동할값이 0 보다 크면서 배열 테두리보다 작아야함
+                    if (map[now_y][now_x] != 0 && !chk[now_y][now_x]) {//맵의 값이 0 아니면서 방문한적이 없으면
+                        queue.add(new Node(now_y, now_x));//큐에 해당 위치 값 삽입
+                        chk[now_y][now_x] = true;//방문 표시
+                        map[now_y][now_x] = map[now.y][now.x] + 1;//이동할 칸에 현재 칸의 이동횟수에 +1
+                   }
+                    //방문한적이 없는곳은 줄어가므로 큐가 반복되는건 계속 가다가 사라짐
+                }
+            }
+        }
+
+    }
 }
