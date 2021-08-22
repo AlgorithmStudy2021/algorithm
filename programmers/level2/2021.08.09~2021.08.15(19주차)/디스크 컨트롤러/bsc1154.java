04e31e1a80f3792e7d2d2e9ff6b345ebc2d2f8ab
bsc1154 <khl1154@naver.com>
2021-08-23 02:09:43 +0900

    Update bsc1154.java

--- programmers/level2/2021.08.09~2021.08.15(19주차)/디스크 컨트롤러/bsc1154.java
+++ programmers/level2/2021.08.09~2021.08.15(19주차)/디스크 컨트롤러/bsc1154.java
@@ -1,4 +1,4 @@
-    public int solution(int[][] jobs) {
+    public static int solution(int[][] jobs) {
 
         int answer = 0;
         int end = 0; // 수행되고난 직후의 시간
@@ -15,7 +15,8 @@
         while (count < jobs.length) {
 
             // 하나의 작업이 완료되는 시점(end)까지 들어온 모든 요청을 큐에 넣음
-            while (jobsIdx < jobs.length && jobs[jobsIdx][0] <= end) {
+            //[[0, 3], [1, 9], [2, 6]]
+            while (jobsIdx < jobs.length && jobs[jobsIdx][0] <= end) {//작업개수 이하이면서 && 시작시점이 수행되고난 직후의 시간 이하일경우 어떤 요청도 들어올수있음
                 pq.add(jobs[jobsIdx++]);
             }
 
@@ -23,13 +24,17 @@
             // (end를 요청의 가장 처음으로 맞춰줌)
             if (pq.isEmpty()) {
                 end = jobs[jobsIdx][0];
-
                 // 작업이 끝나기 전(end 이전) 들어온 요청 중 가장 수행시간이 짧은 요청부터 수행
             } else {
-
-                int[] temp = pq.poll();
-                answer += temp[1] + end - temp[0];
-                end += temp[1];
+                System.out.println("=====================");
+                int[] temp = pq.poll();//가장 윗수를 꺼내서 저장
+                System.out.println(temp);
+                answer += temp[1] + end - temp[0];//처리시간 + 현재시간 - 요청시간
+                System.out.println("temp[1] : "+temp[1]);
+                System.out.println("end : "+end);
+                System.out.println("temp[0] : "+temp[0]);
+                end += temp[1];//현재 시간에는 처리시간을 더해주고
+                System.out.println(end);
                 count++;
             }
         }
